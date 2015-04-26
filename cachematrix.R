a0c703169e9128ca8a64e90423512d501d801934
unknown <dstrey@LH7U05CG5073VMW.ms.ds.uhc.com>
2015-04-26 16:39:17 -0500

    ## Programming Assignment 02
    Signed-off-by: unknown <dstrey@LH7U05CG5073VMW.ms.ds.uhc.com>

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R functions that are able to cache potentially time-consuming computations
+##  this pair of functions cache the inverse of a matrix
 
-## Write a short comment describing this function
 
+
+## Test matrix
+B = matrix( 
+c(2, 4, 3, 1, 5, 7, 9,0), 
+nrow=3, 
+ncol=3) 
+
+## Test matrix
+B = matrix( 
+c(runif(1, 0, 9)
+, runif(1, 0, 9)
+, runif(1, 0, 9)
+, runif(1, 0, 9)
+, runif(1, 0, 9)
+, runif(1, 0, 9)
+, runif(1, 0, 9)
+, runif(1, 0, 9)
+, runif(1, 0, 9)
+
+), 
+nrow=3, 
+ncol=3) 
+
+
+## This function creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+        	m <<- solve(x)
+        	m
 }
+makeCacheMatrix(B)
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the 
+## special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), 
+## then cacheSolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		if(!is.null(m)) {
+                	message("getting cached data")
+                	return(m)
+        	}
+	else solve (B)
 }
+
+cacheSolve(B)
+
+
+
+
+
+
+
+
+
+
+
